8d4f2f0edd36c18913ff363e123224f548e5664a
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2013-10-22 23:46:09 +0300

    Add test skeleton for timelinegrid

---
+++ test/test-timelinegrid.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+var timelinegrid = require("../src/js/timelinegrid");
+
+describe('timelinegrid', function() {
+    it('basic', function() {
+
+        var timeline = timelinegrid.TimelineGrid({
+
+        });
+    });
+});
